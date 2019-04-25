2733a4dc1fe32a075467b841d2f932bb84c6da39
Daneel <kenp4rk@ya.ru>
2019-04-25 22:43:08 +0300

    Extra Task is done!

---
+++ Lesson 2/extra task/index.js
@@ -0,0 +1,21 @@
+let weekDay = ['Monday','Tuesday','Wednesday','Thursday','Friday','Saturday','Sunday'];
+
+document.writeln(  
+    weekDay[0] + '<br>',
+    weekDay[1] + '<br>',
+    weekDay[2] + '<br>',
+    '<em>' + weekDay[3] + '</em>' + '<br>',
+    weekDay[4] + '<br>',
+    '<strong>' + weekDay[5] + '</strong>' + '<br>',
+    '<strong>' + weekDay[6] + '</strong>' + '<br>'
+);
+
+let arr = ['345','874','178','567','890','777','999'];
+console.log( arr[1] + ' ' + arr[5] );
+
+
+
+
+
+
+
